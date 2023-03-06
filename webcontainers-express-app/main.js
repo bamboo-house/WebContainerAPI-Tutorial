dcbbfe862157ac30452a897e76959f18c2b7c8fd
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-03-06 20:16:32 +0900

    ターミナルの画面を追加

--- webcontainers-express-app/main.js
+++ webcontainers-express-app/main.js
@@ -1,12 +1,23 @@
 import "./style.css";
 import { WebContainer } from "@webcontainer/api";
 import { files } from "./files.js";
+import { Terminal } from "xterm";
+import "xterm/css/xterm.css";
 
 /** @type {import('@webcontainer/api').WebContainer}  */
 let webcontainerInstance;
 
 window.addEventListener("load", async () => {
   textareaEl.value = files["index.js"].file.contents;
+  // textareaの内容が変更されたら、index.jsを書き換える
+  textareaEl.addEventListener("input", (e) => {
+    writeIndexJS(e.target.value);
+  });
+
+  const terminal = new Terminal({
+    convertEol: true,
+  });
+  terminal.open(terminalEl);
 
   // WebContainerを起動する
   webcontainerInstance = await WebContainer.boot();
@@ -17,6 +28,9 @@ window.addEventListener("load", async () => {
   if (exitCode !== 0) {
     throw new Error("Installation failed");
   }
+
+  // WebContainerのexpressサーバーを起動する
+  startDevServer();
 });
 
 document.querySelector("#app").innerHTML = `
@@ -28,6 +42,7 @@ document.querySelector("#app").innerHTML = `
       <iframe src="loading.html"></iframe>
     </div>
   </div>
+  <div class="terminal"></div>
 `;
 
 /** @type {HTMLIFrameElement | null} */
@@ -35,6 +50,9 @@ const iframeEl = document.querySelector("iframe");
 
 /** @type {HTMLTextAreaElement | null} */
 const textareaEl = document.querySelector("textarea");
+
+/** @type {HTMLTextAreaElement | null} */
+const terminalEl = document.querySelector(".terminal");
 
 async function installDependencies() {
   const installProcess = await webcontainerInstance.spawn("npm", ["install"]);
@@ -50,9 +68,26 @@ async function installDependencies() {
 }
 
 async function startDevServer() {
-  await webcontainerInstance.spawn("npm", ["run", "start"]);
+  const startProcess = await webcontainerInstance.spawn("npm", [
+    "run",
+    "start",
+  ]);
+
+  // startProcess.output.pipeTo(
+  //   new WritableStream({
+  //     write(data) {
+  //       console.log(data);
+  //     },
+  //   })
+  // );
 
   webcontainerInstance.on("server-ready", (port, url) => {
     iframeEl.src = url;
   });
+}
+
+/** @param {string} content */
+
+async function writeIndexJS(content) {
+  await webcontainerInstance.fs.writeFile("/index.js", content);
 }
